03bd32bde3acc7dab2bdc1aa18826518928d32bc
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-07-06 15:26:25 +0530

    Add dark mode functionality

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react';
+import { MdLightMode, MdDarkMode } from 'react-icons/md'
+
+const DarkMode = () => {
+    const { theme, setTheme, systemTheme } = useTheme();
+    const [mounted, setmounted] = useState(false)
+    const currentTheme = theme == 'system' ? systemTheme : theme
+    useEffect(() => {
+        setmounted(true)
+    }, [])
+    return (
+        <div>
+            {
+                mounted && (currentTheme == "dark" ? <MdLightMode onClick={() => setTheme("light")}
+                    className='text-xl hover:text-blue-400 cursor-pointer'
+                /> : <MdDarkMode onClick={() => setTheme("dark")}
+                    className='text-xl hover:text-blue-400 cursor-pointer'
+                />
+                )}
+        </div>
+    )
+}
+
+export default DarkMode
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import { AiFillHome } from 'react-icons/ai'
 import { BsFillInfoCircleFill } from 'react-icons/bs'
 import Link from 'next/link'
+import DarkMode from './DarkMode'
 const Header = () => {
     return (
         <div className='flex justify-between items-center p-3 max-w-6xl mx-auto '>
@@ -11,10 +12,15 @@ const Header = () => {
                 <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
             </div>
 
-            <Link href={"/"} className='flex gap-1 items-center'>
-                <span className='text-2xl font-bold bg-blue-400 py-1 px-2 rounded-lg'>IMDb</span>
-                <span className='text-xl hidden sm:inline'>Clone</span>
-            </Link>
+
+            <div className=' flex items-center gap-4'>
+                <DarkMode />
+
+                <Link href={"/"} className='flex gap-1 items-center'>
+                    <span className='text-2xl font-bold bg-blue-400 py-1 px-2 rounded-lg'>IMDb</span>
+                    <span className='text-xl hidden sm:inline'>Clone</span>
+                </Link>
+            </div>
         </div>
     )
 }
